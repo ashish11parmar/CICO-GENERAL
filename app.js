7f5b2ebc7abe719c9f8475e20ba589ad2fb1b123
ashishmp <parmarashish1105@gmail.com>
2024-02-15 19:36:30 +0530

    add new column in schema for otp expire and formate code

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
+
 const db = require('./db/conn');
 const PORT = process.env.PORT;
 const cors = require("cors");
+const authRoute = require('./router/auth-router');
+const employeeRoute = require('./router/employee-router');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(require('./router/auth-router'));
-app.use(require('./router/employee-router'));
+app.use('/api', authRoute);
+app.use('/employee', employeeRoute);
 
 app.listen(PORT, () => {
     console.log(`${PORT} is running`);
