20f00fa55a2f755a0b7f2c91402d02fab08a1e1a
keval vala <kevalvala.rao@gmail.com>
2024-02-19 18:50:53 +0530

    forgot password is pending

--- router/auth-router.js
+++ router/auth-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { userSignup, userLogin, verifyOTP, resendOtp } = require('../controllers/employee-controller');
+const { userSignup, userLogin, verifyOTP, resendOtp, forgotPassword } = require('../controllers/employee-controller');
 const router = express.Router();
 
 // routes for register and login 
@@ -7,5 +7,6 @@ router.post('/signup', userSignup)
 router.post('/signin', userLogin);
 router.post('/verifyotp', verifyOTP)
 router.post('/resentotp', resendOtp)
+router.post('/forgotpassword', forgotPassword)
 
 module.exports = router;
