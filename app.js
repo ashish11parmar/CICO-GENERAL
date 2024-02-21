c80b3aa51e8935c62133358a008093e8b1117dc1
ashishmp <parmarashish1105@gmail.com>
2024-02-21 11:54:58 +0530

    added body parser

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const db = require('./db/conn');
 const indexRouter = require('./router/index');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT;
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 //_______MainRoute________//
 app.use('/', indexRouter)
